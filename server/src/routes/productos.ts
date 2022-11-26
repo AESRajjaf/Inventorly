230cf573b22e2826bf663c5c1c46a9bbf3feb4e7
AESRajjaf <119108419+AESRajjaf@users.noreply.github.com>
2022-11-26 15:05:23 +0100

    Update products routes

--- server/src/routes/productos.ts
+++ server/src/routes/productos.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
+import {
+  getProducts,
+  getProduct,
+  addProduct,
+  editProduct,
+  deleteProduct,
+} from "../controllers/productsController";
+import checkAuth from "../middleware/checkAuth";
 
 const router = Router();
+
+router.route("/").get(checkAuth, getProducts).post(checkAuth, addProduct);
+
+router
+  .route("/:id")
+  .get(checkAuth, getProduct)
+  .put(checkAuth, editProduct)
+  .delete(checkAuth, deleteProduct);
 
 export { router };
