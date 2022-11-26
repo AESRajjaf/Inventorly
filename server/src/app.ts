83101af7befb76c8a9eee3feffbc8a6d12ee9012
sohayeb31130 <sohayeb31130@gmail.com>
2022-11-26 17:56:27 +0100

    Update Last

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Cors
 
-const whitelist = [process.env.FRONTEND_URL];
+const whitelist = [process.env.CLIENT_URL];
 
 const corsOptions = {
   origin: function (origin: any, callback: any) {
