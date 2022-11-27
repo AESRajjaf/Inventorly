aec58fd18f1eb8bd6c91bd2ff057d73ba31b83ae
sohayeb31130 <sohayeb31130@gmail.com>
2022-11-27 21:16:36 +0100

    Ui Login

--- client/src/components/AuthFormCard/AuthFormCard.tsx
+++ client/src/components/AuthFormCard/AuthFormCard.tsx
@@ -2,11 +2,16 @@
 import { useState } from "react";
 import styles from "./AuthFormCard.module.css";
 import Logo from "../../assets/Logo_2.svg";
+import MessageIcon from "../../assets/Icons/Message.svg";
+import LockIcon from "../../assets/Icons/Lock.svg";
+import EyeOnIcon from "../../assets/Icons/EyeOn.svg";
+import EyeOffIcon from "../../assets/Icons/EyeOff.svg";
 import clientAxios from "../../config/clientAxios";
 
 const AuthFormCard = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
@@ -31,24 +36,46 @@ const AuthFormCard = () => {
 
   return (
     <div className={styles.card}>
-      <img className={styles.logo} src={Logo} />
+      <div className={styles.header}>
+        <img className={styles.logo} src={Logo} />
+      </div>
       <div className={styles.form}>
         <h3>Bienvenid@ de nuevo</h3>
-        <p>hello</p>
+        <p>Iniciar Sesión</p>
         <form>
-          <input
-            type="text"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={onSubmit}>BOTONA</button>
+          <div className={styles.inputGroup}>
+            <img className={styles.inputIcon} src={MessageIcon} />
+            <input
+              type="text"
+              placeholder="Correo electrónico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <img className={styles.inputIcon} src={LockIcon} />
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <img
+              className={styles.passwordIcon}
+              src={showPassword ? EyeOnIcon : EyeOffIcon}
+              onClick={() => setShowPassword(!showPassword)}
+            />
+            <a className={styles.forgotPassword} href="#">
+              Restablecer contraseña
+            </a>
+          </div>
+
+          <button className={styles.submitBtn} onClick={onSubmit}>
+            Iniciar Sesión
+          </button>
+          <a className={styles.registerLink} href="#">
+            ¿No tienes una cuenta? <span>Contáctanos</span>
+          </a>
         </form>
       </div>
     </div>
